2b00b13a41d09e6c1a908f5e42bfe6eb2b6b42c4
AlinaGrishko1230 <kannakurenai@gmail.com>
2024-07-27 15:14:45 +0300

    Сделала хедер

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,37 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Магазин NFT</title>
+    <title>Цветочная лавка</title>
     <link rel="stylesheet" href="{{ asset('sccs/index.css') }}">
 </head>
 <body>
+<header class="header">
+        <div class="info">
+            <div class="header_left">
+                <div class="target">
+                    <img src="image/headerimg/Target.png" alt="target">
+                </div>
+                <div class="address">
+                    <span class="city">г. Владивосток,</span>
+                    <span class="street"> ул. Пушкинская, 17 А</span>
+                </div> 
+            </div>
+        </div>
+            <div class="header_center">
+                <div>
+                    <img class="handset" src="image/headerimg/handset.png" alt="handset">
+                        <div class="container_number"> 
+                            <span class="whatsapp">whatsapp</span>
+                            <span class="number">+7-888-888-8888 +7-777-777-7777</span>
+                        </div>
+                </div>
+            </div>
+        <div class="info">
+            <div class="header_right">
+                <a href="#">
+                <img src="image/headerimg/Vector.png" alt="cart">
+                </a>
+            </div>
+        </div>
+    </header>
 </body>
