f0bbfe201b2a56583f98b3c098aae7bcea933110
v.grishko <v.grishko@novostroy-m.ru>
2024-07-29 00:13:18 +0300

    Сделал адаптивный футер и перенес хедер в отдельный шаблон

---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,51 @@
+<header class="header">
+    <div class="info">
+        <div class="header_left">
+            <div class="target">
+                <img src="image/headerimg/Target.png" alt="target">
+            </div>
+            <div class="address">
+                <span class="city">г. Владивосток,</span>
+                <span class="street"> ул. Пушкинская, 17 А</span>
+            </div>
+        </div>
+    </div>
+    <div class="header_center">
+        <div>
+            <img class="handset" src="image/headerimg/handset.png" alt="handset">
+            <div class="container_number">
+                <span class="whatsapp">whatsapp</span>
+                <span class="number">+7-888-888-8888 +7-777-777-7777</span>
+            </div>
+        </div>
+    </div>
+    <div class="infocart">
+        <div class="header_right">
+            <a href="#" >
+                <img src="image/headerimg/Vector.png" alt="cart">
+                <span class="Sum">150000₽</span>
+            </a>
+        </div>
+    </div>
+</header>
+<div class="navbar">
+    <div class="navbar_left">
+        <a href="#" class="catalog">Каталог</a>
+        <a href="#" class="reviews">Отзывы</a>
+        <a href="#" class="сontacts">Контакты</a>
+    </div>
+    <div class="navbar_logo">
+        <img src="image/navbar/flover_shop.png" alt="flover_shop">
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Информация для клиента</button>
+        <div class="dropdown-content">
+            <a href="#">Скидки</a>
+            <a href="#">Акции</a>
+            <a href="#">Статус заказа</a>
+        </div>
+    </div>
+    <div class="navbar_right">
+        <a href="#">Мои заказы</a>
+    </div>
+</div>
