08207b59ecdfab3e491675d0435bcfcbe4c12eee
AlinaGrishko1230 <kannakurenai@gmail.com>
2024-08-05 16:00:36 +0300

    правки

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Цветочная лавка</title>
     <link rel="stylesheet" href="{{ asset('sccs/header/index.css') }}">
-    <link rel="stylesheet" href="{{asset('sccs/slider/index.css') }}">
+    <link rel="stylesheet" href="{{asset('sccs/home/index.css') }}">
     <link rel="stylesheet" href="{{ asset('sccs/footer/index.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
     @include('template.header')
-    @include('template.slider')
+    <div class="slider">
+        <img src="image/slider/slider_one.png" alt="slider_one">
+    </div>
+        <div class="title_slider">
+            <div class="delivery">
+                <span class="delivery_span">Доставка</span>
+                <span> цветов в городе</span>
+            </div>
+                <p class="Vladivostok">Владивосток</p>
+                <button class="catalog_button">Каталог</button>
+        </div>
     @include('template.footer')
 </body>
